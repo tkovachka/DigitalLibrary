5e369584216a3aa14e57a6a9b66184ca813a5a08
Teodora Kovachka <teodora.kovachka@students.finki.ukim.mk>
2026-03-28 18:34:34 +0100

    feat: implement account management and password change + new frontend design

---
+++ LibraryApplication.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,135 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using LibraryApplication.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryApplication.Web.Areas.Identity.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<LibraryApplicationUser> _userManager;
+        private readonly SignInManager<LibraryApplicationUser> _signInManager;
+
+        public IndexModel(
+            UserManager<LibraryApplicationUser> userManager,
+            SignInManager<LibraryApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+
+            [Display(Name = "Surname")]
+            public string Surname { get; set; }
+
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+        }
+
+        private async Task LoadAsync(LibraryApplicationUser user)
+        {
+            Username = await _userManager.GetUserNameAsync(user);
+            Email = await _userManager.GetEmailAsync(user);
+
+            Input = new InputModel
+            {
+                Name = user.Name,
+                Surname = user.Surname,
+                PhoneNumber = await _userManager.GetPhoneNumberAsync(user)
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            // Update phone
+            var currentPhone = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != currentPhone)
+            {
+                var phoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+
+                if (!phoneResult.Succeeded)
+                {
+                    StatusMessage = "Error: Failed to update phone number.";
+                    return RedirectToPage();
+                }
+            }
+
+            // Update custom fields
+            bool changed = false;
+
+            if (user.Name != Input.Name)
+            {
+                user.Name = Input.Name;
+                changed = true;
+            }
+
+            if (user.Surname != Input.Surname)
+            {
+                user.Surname = Input.Surname;
+                changed = true;
+            }
+
+            if (changed)
+            {
+                var updateResult = await _userManager.UpdateAsync(user);
+
+                if (!updateResult.Succeeded)
+                {
+                    StatusMessage = "Error: Failed to update profile.";
+                    return RedirectToPage();
+                }
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+
+            StatusMessage = "Your profile has been updated successfully.";
+            return RedirectToPage();
+        }
+    }
+}
